8f8aa9a9643636fea4a268651ddbad20a776ad98
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-04-11 10:44:31 +0200

    Update tablicaAsocjacyjnaNaNULLorazCiagWartosci.php

--- tablicaAsocjacyjnaNaNULLorazCiagWartosci.php
+++ tablicaAsocjacyjnaNaNULLorazCiagWartosci.php
@@ -1,17 +1,23 @@
 <?php
 class tablicaAsocjacyjnaNaNULLorazCiagWartosci{
-	public function tablicaDoBazy($tablica=array()){
-	$ciag="(NULL, '".implode("','", $tablica)."')";
-	return $ciag;
+	public function tablicaDoBazy($tab=array()){
+		$tabBezId=array();
+foreach($tab as $klucz=>$wartosc)
+if($klucz!="id")
+	$tabBezId[$klucz]=$tab[$klucz];
+$ciag="(NULL, '".implode("','", $tabBezId)."')";
+return $ciag;
 	}
 }
+/*Jeśli istnieje $tab["id"] to nie będzie w ciągu.*/
 
 /*
-$tab["x"]="v1";
+$tab["id"]=55;
+$tab["x"]="v11";
 $tab["y"]="v2";
 $tab["z"]="v3";
 $tablicaAsocjacyjnaNaNULLorazCiagWartosci=new tablicaAsocjacyjnaNaNULLorazCiagWartosci();
 $t=$tablicaAsocjacyjnaNaNULLorazCiagWartosci->tablicaDoBazy($tab);
-echo $t;
-*/
+echo $t;*/
+
 ?>
